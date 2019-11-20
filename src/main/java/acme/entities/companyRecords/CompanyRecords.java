afaec06b13e4eaa595ec8d8aa1ad1a56187641b4
javnavpli <56296738+javnavpli@users.noreply.github.com>
2019-11-20 20:27:40 +0100

    Update CompanyRecords.java

--- src/main/java/acme/entities/companyRecords/CompanyRecords.java
+++ src/main/java/acme/entities/companyRecords/CompanyRecords.java
@@ -4,12 +4,11 @@ package acme.entities.companyRecords;
 import javax.persistence.Entity;
 import javax.persistence.Transient;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
+import org.hibernate.validator.constraints.Range;
 import org.hibernate.validator.constraints.URL;
 
 import acme.framework.entities.DomainEntity;
